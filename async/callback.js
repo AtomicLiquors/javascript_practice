68b1ce6cdd17c1e9f4814ca3c86114fd13055300
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2024-07-06 12:53:34 +0900

    callback

--- async/callback.js
+++ async/callback.js
@@ -1,12 +1,51 @@
 'use strict';
 // JavaScript is synchronous.
 // Execute the code block by in order after hoisting.
-// hoisting: var, function declaration
 
-console.log(1);
-setTimeout(() => console.log(2), 1000);
-console.log(3);
 
-// Synchronous callback
+// console.log(1);
+// setTimeout(() => console.log(2), 1000);
+// console.log(3);
 
-// Asynchronous callback
+
+class UserStorage {
+    loginUser(id, password, onSuccess, onError){
+        setTimeout(()=>{
+            if(
+                (id === '2kooong2' && password === '2222')
+            ){
+                onSuccess(id);
+            }else{
+                onError(new Error('not found'));
+            }
+        }, 2000);
+    }
+
+    getRoles(user, onSuccess, onError){
+        setTimeout(()=>{
+            if(
+                (user === '2kooong2')
+            ){
+                onSuccess({name: '2kooong2', role: 'man who smiles'});
+            }else{
+                onError(new Error('no access'));
+            }
+        }, 1000);
+    }
+}
+
+const userStorage = new UserStorage();
+const id = '2kooong2';
+const password = '2222';
+
+userStorage.loginUser('2koooong2', '2222', user => {
+    userStorage.getRoles(
+        user, 
+        userWithRole => {
+            console.log(`Welcome ${userWithRole.name}, ${userWithRole.role}`);
+        },
+        error => {
+            console.log(error);
+        }
+    );
+});
